d337375dd994fa4eb8ce722fd8eb65866d6b1d90
Collin Richards <richardscollin@gatech.edu>
2014-11-11 20:08:30 -0500

    hello mongo

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,35 @@
 'use strict';
 
 var express = require('express');
+var mongoose = require('mongoose');
 var router = express.Router();
 
 router.get('/', function(req, res) {
     res.send({
         data: "Hello world",
         status: "success"
+    });
+});
+
+var ListItem = mongoose.model('listItem', mongoose.Schema({
+    name: String,
+    description: String
+}));
+
+router.get('/hello/:id', function(req, res) {
+    var lastItem = new ListItem({ name: req.params['id'] });
+    lastItem.save(function (err) {
+        if (err) {
+            res.send({
+                message: err,
+                status: "Error"
+            });
+        } else {
+            res.send({
+                data: "Hello world",
+                status: "success"
+            });
+        }
     });
 });
 
