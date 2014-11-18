69144f8edaa45090f976e53710c12cc7a12cdece
Kelvin Chen <kelvin@kelvinchen.org>
2014-11-18 01:15:52 -0500

    Add server side rendering

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var routes = require('./routes');
 var api = require('./routes/api');
 
 var app = express();
@@ -23,9 +24,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', routes);
 app.use('/api', api);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
