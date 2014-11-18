678034cc35a5396e313ac350cdc7474e91ee4183
Kelvin Chen <kelvin@kelvinchen.org>
2014-11-18 15:27:04 -0500

    Change some names

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var express = require('express'),
     router = express.Router();
 
 router.get('/', function(req, res) {
-    res.render('index');
+    res.render('home');
 });
 
 module.exports = exports = router;
