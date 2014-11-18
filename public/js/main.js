e4a5b282a4acb613dd083ce7a1f410afdfe526bd
Kelvin Chen <kelvin@kelvinchen.org>
2014-11-18 15:22:53 -0500

    Work on main layout

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('app', [
+    'ui.bootstrap',
+    'app.controllers',
+    'app.directives'
+]);
+
+angular.module('app.controllers', []);
+angular.module('app.directives', []);
